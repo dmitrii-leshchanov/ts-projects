d33feb3abe973399bfc84ac25aad88452eab6af2
Dmitrii Leshchanov <leshok.dimkaa@gmail.com>
2023-01-07 14:58:56 +0200

    HW# 30

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
 "use strict";
-function getRandomNumber(min, max) {
-    return Math.trunc(min + Math.random() * (max - min + 1));
+var firstLetter = 'a'.charCodeAt(0);
+var lastLetter = 'z'.charCodeAt(0);
+function shiftCipher(cipher, shift) {
+    if (shift === void 0) { shift = 1; }
+    return cipherTransition(cipher, shift);
 }
-let min = 1;
-let max = 200;
-console.log(getRandomNumber(min, max));
+function shiftDecipher(cipher, shift) {
+    if (shift === void 0) { shift = 1; }
+    return cipherTransition(cipher, -shift);
+}
+function cipherTransition(cipher, shift) {
+    shift = shift % (lastLetter - firstLetter);
+    var cipherArray = cipher.split('');
+    cipherArray.forEach(function (letter, index, array) {
+        var codeAscii = letter.charCodeAt(0);
+        if (codeAscii >= firstLetter && codeAscii <= lastLetter) {
+            codeAscii = codeAscii + shift;
+            if (codeAscii > lastLetter) {
+                codeAscii = firstLetter + (codeAscii - lastLetter);
+            }
+            else if (codeAscii < firstLetter) {
+                codeAscii = lastLetter - (firstLetter - codeAscii);
+            }
+            array[index] = String.fromCharCode(codeAscii);
+        }
+    });
+    return cipherArray.join('');
+}
+var message = 'Abdg132';
+var shift = 105; // 105 % 25 = 5;
+var shiftCipherResult = shiftCipher(message, shift);
+var shiftDecipherResult = shiftDecipher(shiftCipherResult, shift);
+var shiftCipherResultDefault = shiftCipher(message); // Shift default = 1;
+var shiftDecipherResultDefault = shiftDecipher(shiftCipherResultDefault);
+console.log('Message ', message, shift, lastLetter - firstLetter);
+console.log("Ciphered message with shift = ".concat(shift, ": "), shiftCipherResult);
+console.log("Deciphered message  with shift = ".concat(shift, ": "), shiftDecipherResult);
+console.log('Ciphered message with default shift: ', shiftCipherResultDefault);
+console.log('Deciphered message with default shift: ', shiftDecipherResultDefault);
--- app.ts
+++ app.ts
@@ -1,8 +1,42 @@
-function getRandomNumber(min: number, max: number):number  {
-    return Math.trunc(min + Math.random() * (max - min + 1));
+const firstLetter: number = 'a'.charCodeAt(0);
+const lastLetter: number = 'z'.charCodeAt(0);
+
+function shiftCipher(cipher: string, shift: number = 1): string {
+    return cipherTransition(cipher, shift);
 }
 
-let min = 1;
-let max = 200;
+function shiftDecipher(cipher: string, shift: number = 1): string {
+    return cipherTransition(cipher, -shift);
+}
 
-console.log(getRandomNumber(min, max));
+function cipherTransition(cipher: string, shift: number): string {
+    shift = shift % (lastLetter - firstLetter);
+    const cipherArray = cipher.split('');
+    cipherArray.forEach((letter: string, index: number, array: string[]) => {
+        let codeAscii = letter.charCodeAt(0);
+        if(codeAscii >= firstLetter && codeAscii <= lastLetter) {
+            codeAscii = codeAscii + shift;
+            if(codeAscii > lastLetter) {
+                codeAscii = firstLetter + (codeAscii - lastLetter);
+            } else if (codeAscii < firstLetter) {
+                codeAscii = lastLetter - (firstLetter - codeAscii);
+            }
+            array[index] = String.fromCharCode(codeAscii);
+        }
+    })
+
+    return cipherArray.join('');
+}
+
+const message: string = 'Abdg132';
+const shift: number = 105; // 105 % 25 = 5;
+const shiftCipherResult: string = shiftCipher(message, shift);
+const shiftDecipherResult: string = shiftDecipher(shiftCipherResult, shift);
+const shiftCipherResultDefault: string = shiftCipher(message); // Shift default = 1;
+const shiftDecipherResultDefault: string = shiftDecipher(shiftCipherResultDefault);
+
+console.log('Message ', message, shift, lastLetter-firstLetter);
+console.log(`Ciphered message with shift = ${shift}: `, shiftCipherResult);
+console.log(`Deciphered message  with shift = ${shift}: `, shiftDecipherResult);
+console.log('Ciphered message with default shift: ', shiftCipherResultDefault);
+console.log('Deciphered message with default shift: ', shiftDecipherResultDefault);
